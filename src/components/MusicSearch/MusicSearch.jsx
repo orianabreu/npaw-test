30ffa4aee17fdd9afdc34b22b575b59e9cc43c8c
Oriana Abreu <oriabreu.v@gmail.com>
2022-04-13 01:53:03 +0200

    handle errors and set loading

--- src/components/MusicSearch/MusicSearch.jsx
+++ src/components/MusicSearch/MusicSearch.jsx
@@ -6,16 +6,29 @@ export default function MusicSearch() {
   const [searchResults, setSearchResults] = useState([]);
   const [inputSearch, setInputSearch] = useState("");
   const [searchDone, setSearchDone] = useState(false);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const url = `https://itunes.apple.com/search?term=${inputSearch}&entity=album&limit=20`;
 
   const searchMusic = async () => {
+    if (!inputSearch) {
+        console.log("error");
+        setError("Please, add a valid search album name or artist")
+    } else {
+        setError(null);
+    }
+
+    setIsLoading(true);
+
     try {
       const response = await axios(url);
       console.log(response.data.results);
       setSearchResults(response.data.results);
+      setIsLoading(false);
     } catch (error) {
-      console.log(error);
+      console.log(error.message);
+      setError(error.message);
     }
     setSearchDone(true);
   };
@@ -27,7 +40,9 @@ export default function MusicSearch() {
   return (
     <div>
       <input type='text' value={inputSearch} onChange={handleChange} />
-      <button onClick={searchMusic}>search</button>
+      <button onClick={searchMusic}>{isLoading ? "Loading..." : "Search"}</button>
+
+      {error && <p>{error}</p>}
 
       {searchDone &&
         searchResults.map((album, index) => {
