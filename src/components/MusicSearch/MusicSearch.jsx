eccbe1c3bb0d8617b44fe19b2a0a1209a8aa42bf
Oriana Abreu <oriabreu.v@gmail.com>
2022-04-13 03:14:33 +0200

    manage state with useReducer

--- src/components/MusicSearch/MusicSearch.jsx
+++ src/components/MusicSearch/MusicSearch.jsx
@@ -1,46 +1,42 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import axios from "axios";
+
 import AlbumCard from "../AlbumCard/AlbumCard";
+import { initialState, reducer } from "./reducer";
 
 export default function MusicSearch() {
-  const [searchResults, setSearchResults] = useState([]);
-  const [inputSearch, setInputSearch] = useState("");
-  const [searchDone, setSearchDone] = useState(false);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { searchResults, inputSearch, searchDone, error } = state;
 
   const url = `https://itunes.apple.com/search?term=${inputSearch}&entity=album&limit=20`;
 
   const searchMusic = async () => {
     if (!inputSearch) {
-        console.log("error");
-        setError("Please, add a valid search album name or artist")
+      console.log("error");
+      dispatch({ type: "empy-inputfield" });
     } else {
-        setError(null);
+      dispatch({ type: "no-error" });
     }
-
-    setIsLoading(true);
 
     try {
       const response = await axios(url);
       console.log(response.data.results);
-      setSearchResults(response.data.results);
-      setIsLoading(false);
+      dispatch({ type: "get-api-data", data: response.data.results });
     } catch (error) {
       console.log(error.message);
-      setError(error.message);
+      dispatch({ type: "error", payload: error.message });
     }
-    setSearchDone(true);
+    dispatch({ type: "show-results" });
   };
 
   const handleChange = (e) => {
-    setInputSearch(e.target.value);
+    dispatch({ type: "search", payload: e.target.value });
   };
 
   return (
     <div>
       <input type='text' value={inputSearch} onChange={handleChange} />
-      <button onClick={searchMusic}>{isLoading ? "Loading..." : "Search"}</button>
+      <button onClick={searchMusic}>search</button>
 
       {error && <p>{error}</p>}
 
